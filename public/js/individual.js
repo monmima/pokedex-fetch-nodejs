4431c8423c25cd6447c2326ea8c9787bd5074619
monmima <monmima@protonmail.com>
2020-10-10 18:41:55 -0400

    améliorations visuelles; meilleure sémantique

--- public/js/individual.js
+++ public/js/individual.js
@@ -6,13 +6,40 @@ function getCurrentPage() {
     return myLocation;
 }
 
+function printEvo(whatArrayEvo) {
+    let pokeName = getCurrentPage();
+    let indexInArray;
+
+    // for (let i = 0; i < whatArrayEvo.length; i++) {
+    //     document.querySelector("main").innerHTML += `<div>${whatArrayEvo[i]}</div>`;
+    // }
+
+    if (pokeName === whatArrayEvo[0] && whatArrayEvo[1]) {
+        document.querySelector("main").innerHTML += `
+            <div>
+                Prochaine évolution: <a title="Lien vers le pokémon" href="${whatArrayEvo[1]}">${whatArrayEvo[1]}</a>
+            </div>
+        `;
+    } else if (pokeName === whatArrayEvo[1] && whatArrayEvo[2]) {
+        document.querySelector("main").innerHTML += `
+            <div>
+                Prochaine évolution: <a title="Lien vers le pokémon" href="${whatArrayEvo[2]}">${whatArrayEvo[2]}</a>
+            </div>
+        `;
+    } else {
+        document.querySelector("main").innerHTML += `<div>Pokémon sans évolution!</div>`;
+    }
+ 
+}
+
 function getOnePokemonInfo(whatPokemon) {
     // main fetch
     fetch(`https://pokeapi.co/api/v2/pokemon/${whatPokemon}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.types[0].type.name);
-        console.log(data.id);
+        console.log(data);
+        // console.log(data.types[0].type.name);
+        // console.log(data.id);
 
         let pokeType;
 
@@ -24,13 +51,17 @@ function getOnePokemonInfo(whatPokemon) {
             console.log(data.types.length);
         }
 
-        document.querySelector("main").innerHTML += 
-        `
-            <div>
-                <img src="${data.sprites.front_default}" alt="${data.name}">
-            </div>
-            <div>${data.name}</div>
-            <div>Type: ${pokeType}</div>
+        document.querySelector("main").innerHTML += `
+            <figure class="card">
+                <img class="image-pokemon" src="${data.sprites.front_default}" alt="${data.name}">
+                <div class="card-body">
+                    <figcaption class="card-title">
+                        ${data.name}
+                    </figcaption>
+                </div>
+            </figure>
+
+            <div>Expérience: ${data.base_experience}</div>
         `;
 
         // outer nested fetch
@@ -44,22 +75,32 @@ function getOnePokemonInfo(whatPokemon) {
             fetch(data2.evolution_chain.url)
             .then(response => response.json())
             .then(data3 => {
+
                 
                 // console.log(data3.chain.species.name);
                 // console.log(data3.chain.evolves_to[0].species.name);
                 // console.log(data3.chain.evolves_to[0].evolves_to[0].species.name);
 
-                let arrayEvo = [
-                    data3.chain.species.name,
-                    data3.chain.evolves_to[0].species.name,
-                ]
+                let arrayEvo;
 
-                // push a third element to the array if it's not undefined
-                if (data3.chain.evolves_to[0].evolves_to[0]) {
-                    arrayEvo.push(data3.chain.evolves_to[0].evolves_to[0].species.name);
-                }
+                if (data3.chain.evolves_to[0]) {
+                    arrayEvo = [
+                        data3.chain.species.name,
+                        data3.chain.evolves_to[0].species.name,
+                    ]
+    
+                    // push a third element to the array if not undefined
+                    if (data3.chain.evolves_to[0].evolves_to[0]) {
+                        arrayEvo.push(data3.chain.evolves_to[0].evolves_to[0].species.name);
+                    }
+                } else {
+                    console.log("Pokémon sans évolution!");
+                    arrayEvo = [];
+                }               
 
                 console.log(arrayEvo);
+
+                printEvo(arrayEvo);
 
                 return arrayEvo;
 
@@ -78,3 +119,4 @@ function getOnePokemonInfo(whatPokemon) {
 
 const currentPokemon = getCurrentPage();
 getOnePokemonInfo(currentPokemon);
+
--- public/js/list.js
+++ public/js/list.js
@@ -10,15 +10,26 @@ function getList(offset, limit) {
         fetch('https://pokeapi.co/api/v2/pokemon/' + i)
         .then(response => response.json())
         .then(data => {
-            console.log(data.types);
+            // console.log(data.types);
     
-            document.querySelector("main").innerHTML += 
-            `<div>
-                <div><img src="${data.sprites.front_default}" alt="${data.name}"></div>
-                <div>
-                    <div><a href="/${data.name}">${data.name}</a></div>
-                </div>
-            </div>`;
+            // document.querySelector("main").innerHTML += 
+            // `<div>
+            //     <div><img src="${data.sprites.front_default}" alt="${data.name}"></div>
+            //     <div>
+            //         <div><a href="/${data.name}">${data.name}</a></div>
+            //     </div>
+            // </div>`;
+
+            document.querySelector("main").innerHTML += `
+                <figure class="card">
+                    <img class="image-pokemon" src="${data.sprites.front_default}" alt="${data.name}">
+                    <div class="card-body">
+                        <figcaption class="card-title">
+                            <a href="/${data.name}">${data.name}</a>
+                        </figcaption>
+                    </div>
+                </figure>
+            `;
     
         })
         .catch(error => console.error(error));
